0ea15acfc7e925be7ecc6f2dbe18247bbd448751
BRANYA <tbd2504@gmail.com>
2023-12-20 16:02:11 +0200

    Change email field class

--- src/accounts/models.py
+++ src/accounts/models.py
@@ -15,7 +15,7 @@ class Profile(models.Model):
 
 
 class User(AbstractBaseUser, PermissionsMixin):
-    email = models.CharField(max_length=255, unique=True)
+    email = models.EmailField(max_length=255, unique=True)
     password = models.CharField(max_length=20)
     is_confirmed_email = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
