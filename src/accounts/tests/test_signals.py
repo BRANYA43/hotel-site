0ca054cf81622353dd4f859e041b6957cceb5cd1
BRANYA <tbd2504@gmail.com>
2023-12-20 15:51:00 +0200

    Add signal to create profile after save user model

---
+++ src/accounts/tests/test_signals.py
@@ -0,0 +1,24 @@
+from django.test import TestCase
+
+from accounts.models import Profile
+from accounts.tests import create_test_user
+
+
+class CreateProfileSignalTest(TestCase):
+    def test_signal_create_profile_after_save_user_model(self):
+        user = create_test_user()
+
+        self.assertEqual(Profile.objects.count(), 1)
+
+        profile = Profile.objects.first()
+
+        self.assertEqual(profile.user.id, user.id)
+
+    def test_signal_create_profile_if_user_model_doesnt_have_it(self):
+        user = create_test_user()
+
+        self.assertEqual(Profile.objects.count(), 1)
+
+        user.save()
+
+        self.assertEqual(Profile.objects.count(), 1)
