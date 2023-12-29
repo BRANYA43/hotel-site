8089bde7c76ea1f04dcc09911368bd501bc28757
BRANYA <tbd2504@gmail.com>
2023-12-29 17:14:25 +0200

    Add admin for models

--- src/rooms/admin.py
+++ src/rooms/admin.py
@@ -1,1 +1,21 @@
-# Register your models here.
+from django.contrib import admin
+
+from rooms.models import Number, Room
+
+
+class NumberInline(admin.TabularInline):
+    model = Number
+    extra = 0
+
+
+@admin.register(Number)
+class NumberAdmin(admin.ModelAdmin):
+    list_display = ['number', 'room', 'is_available']
+    fields = ['number', 'room', 'is_available']
+
+
+@admin.register(Room)
+class RoomAdmin(admin.ModelAdmin):
+    list_display = ['slug', 'type', 'status', 'price', 'single_beds', 'double_beds']
+    fields = ['slug', 'type', 'status', 'price', 'single_beds', 'double_beds']
+    inlines = [NumberInline]
