6d02b662ddf723c09eef63379a45a3f1c50f3e6b
BRANYA <tbd2504@gmail.com>
2023-12-31 13:46:44 +0200

    Add admin for Booking model

--- src/bookings/admin.py
+++ src/bookings/admin.py
@@ -1,1 +1,22 @@
-# Register your models here.
+from django.contrib import admin
+
+from bookings.models import Booking
+
+
+@admin.register(Booking)
+class BookingAdmin(admin.ModelAdmin):
+    list_display = ['uuid', 'user', 'is_paid', 'check_in', 'check_out', 'created']
+    fields = [
+        'uuid',
+        'user',
+        'persons',
+        'type',
+        'is_children',
+        'is_paid',
+        'check_in',
+        'check_out',
+        'rooms',
+        'updated',
+        'created',
+    ]
+    readonly_fields = ['uuid', 'updated', 'created']
